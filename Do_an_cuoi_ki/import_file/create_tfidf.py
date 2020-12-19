4b09005550e440023d395c6ae7033ed6c71e90e9
Hoangthang017 <63088025+Hoangthang017@users.noreply.github.com>
2020-12-20 00:37:53 +0700

    Add files via upload

--- Do_an_cuoi_ki/import_file/create_tfidf.py
+++ Do_an_cuoi_ki/import_file/create_tfidf.py
@@ -60,7 +60,7 @@ def get_similar_articles(q, df, files_path, vectorizer):
   # print(q_vec)
   sim = {}
   # tính toán độ tương đồng
-  for i in range(len(documents_clean)):
+  for i in range(len(files_path)):
     sim[i] = np.dot(df.loc[:, str(i)].values, q_vec) / np.linalg.norm(df.loc[:, str(i)]) * np.linalg.norm(q_vec)
   # print('độ dài ', len(sim))
   # sắp xếp độ tương đồng
